6cf38cb8f59a2d1482d22b4cb006e5f4ed95a628
JOgden99 <josh.ogden.1999@gmail.com>
2021-12-10 10:46:06 -0700

    Prepped for pull request - cloned repo

--- ecflow/spt_extract_plain_table.py
+++ ecflow/spt_extract_plain_table.py
@@ -18,6 +18,7 @@ import multiprocessing as mp
 import subprocess as sp
 import netCDF4 as nc
 import datetime as dt
+from scipy.interpolate import pchip
 import pandas as pd
 import logging
 
@@ -42,18 +43,24 @@ def extract_summary_table(workspace):
     date_string = os.path.split(workspace)[1].replace('.', '')
     full_name = os.path.split(os.path.split(workspace)[0])[1]
     file_name = 'summary_table_{0}_{1}.csv'.format(full_name, date_string)
+    static_path = str(sys.argv[5])
 
     # creating pandas dataframe with return periods
     era_type = str(sys.argv[4])
-    rp_path = glob(os.path.join(os.path.split(workspace)[0], f'return_periods_{era_type}*.nc'))[0]
+    logging.info(f'Workspace {workspace}')
+    rp_path = glob(os.path.join(static_path, era_type, os.path.basename(os.path.split(workspace)[0]), f'*return_periods_{era_type}*.nc*'))[0]
+    logging.info(f'Return Period Path {rp_path}')
     rp_ncfile = nc.Dataset(rp_path, 'r')
 
     # extract values
     rp_comid = rp_ncfile.variables['rivid'][:]
     data = {
         'return_2': rp_ncfile.variables['return_period_2'][:],
+        'return_5': rp_ncfile.variables['return_period_5'][:],
         'return_10': rp_ncfile.variables['return_period_10'][:],
-        'return_20': rp_ncfile.variables['return_period_20'][:]
+        'return_25': rp_ncfile.variables['return_period_25'][:],
+        'return_50': rp_ncfile.variables['return_period_50'][:],
+        'return_100': rp_ncfile.variables['return_period_100'][:]
     }
 
     #  creates dataframe
@@ -63,7 +70,7 @@ def extract_summary_table(workspace):
     try:
         with open(os.path.join(workspace, file_name), 'w') as f:
             # writes header
-            # f.write('comid,timestamp,max,mean,color,thickness\n')
+            # f.write('comid,timestamp,max,mean,color,thickness,ret_per\n')
 
             # extracts forecast COMIDS and formatted dates into lists
             comids = nc.Dataset(nclist[0], 'r').variables['rivid'][:].tolist()
@@ -71,6 +78,15 @@ def extract_summary_table(workspace):
             dates = []
             for date in rawdates:
                 dates.append(dt.datetime.utcfromtimestamp(date).strftime("%m/%d/%y %H:%M"))
+            # creates a time series of 3H intervals for interpolation, coerces a copy to appropriate type for inserting to file
+            interp_x = pd.date_range(
+                start=dates[0],
+                end=dates[-1],
+                freq='3H'
+            )
+            interp_x_strings = interp_x.strftime("%m/%d/%y %H:%M")
+            new_dates = list(interp_x_strings)
+            dt_dates = pd.to_datetime(dates)
 
             # creates empty lists with forecast stats
             maxlist = []
@@ -80,18 +96,31 @@ def extract_summary_table(workspace):
             for ncfile in sorted(nclist):
                 res = nc.Dataset(ncfile, 'r')
 
-                # loops through COMIDs with netcdf files
+                # loops through COMIDs with netcdf files, adds maxes and means
+                # originally took a slice of the first 49 to only include high res,
+                # but now extracts the whole forecast so the interpolator can fill
+                # in the gaps.
                 for index, comid in enumerate(comids):
                     if 'max' in ncfile:
-                        maxlist.append(res.variables['Qout'][index, 0:49].tolist())
+                        maxlist.append(res.variables['Qout'][index, :].tolist())
                     elif 'avg' in ncfile:
-                        meanlist.append(res.variables['Qout'][index, 0:49].tolist())
+                        meanlist.append(res.variables['Qout'][index, :].tolist())
+            #loops through the lists of max lists and mean lists to interpolate using the dates as x values
+            for index, maxes, means in enumerate(zip(maxlist, meanlist)):
+                max_interpolator = pchip(dt_dates, maxes)
+                mean_interpolator = pchip(dt_dates, means)
+                int_max = max_interpolator(interp_x)
+                int_mean = mean_interpolator(interp_x)
+                maxlist[index] = int_max
+                meanlist[index] = int_mean
+
+
 
             # loops through COMIDs again to add rows to csv file
             for index, comid in enumerate(comids):
-                for f_date, f_max, f_mean in zip(dates, maxlist[index], meanlist[index]):
+                for f_date, f_max, f_mean in zip(new_dates, maxlist[index], meanlist[index]):
                     # define reach color based on return periods
-                    if f_mean > rp_df.loc[comid, 'return_20']:
+                    if f_mean > rp_df.loc[comid, 'return_50']:
                         color = 'purple'
                     elif f_mean > rp_df.loc[comid, 'return_10']:
                         color = 'red'
@@ -114,21 +143,57 @@ def extract_summary_table(workspace):
                     else:
                         thickness = '6'
 
-                    f.write(','.join([str(comid), f_date, str(f_max), str(f_mean), color, thickness + '\n']))
-
+                    # define return period exceeded by the mean forecast
+                    if f_mean > rp_df.loc[comid, 'return_100']:
+                        ret_per = '100'
+                    elif f_mean > rp_df.loc[comid, 'return_50']:
+                        ret_per = '50'
+                    elif f_mean > rp_df.loc[comid, 'return_25']:
+                        ret_per = '25'
+                    elif f_mean > rp_df.loc[comid, 'return_10']:
+                        ret_per = '10'
+                    elif f_mean > rp_df.loc[comid, 'return_5']:
+                        ret_per = '5'
+                    elif f_mean > rp_df.loc[comid, 'return_2']:
+                        ret_per = '2'
+                    else:
+                        ret_per = '0'
+                f.write(','.join([str(comid), f_date, str(f_max), str(f_mean), color, thickness, ret_per + '\n']))
+        f.close()
         return 'Stat Success'
     except Exception as e:
+        f.close()
         logging.debug(e)
+        print(e)
 
 
 # runs function on file execution
 if __name__ == "__main__":
+    """
+        Arguments:
+
+        1. Absolute path to the rapid-io/output directory ???
+        2. Path to the logging file (not used)
+        3. NCO command to compute ensemble stats ???
+        4. era_type: == 'era_5' ???
+        5. static_path === absolute path to the historical simulation data?
+
+        Example Usage
+
+            python spt_extract_plain_table.py arg1 arg2 arg3 arg4 arg5
+
+            python spt_extract_plain_table.py /path/to/rapid-io/output
+
+        """
+
+    logging.basicConfig(filename=str(sys.argv[2]), level=logging.DEBUG)
+    logging.basicConfig(level=logging.DEBUG)
+
     # output directory
     workdir = str(sys.argv[1])
-
     # list of watersheds
     watersheds = [os.path.join(workdir, d) for d in os.listdir(workdir) if os.path.isdir(os.path.join(workdir, d))]
-
+    logging.debug(watersheds)
     dates = []
     exclude_list = []
     for i in range(len(watersheds)):
@@ -136,9 +201,8 @@ if __name__ == "__main__":
             if not any(excluded in watersheds[i] for excluded in exclude_list) and os.path.isdir(
                     os.path.join(watersheds[i], d)):
                 dates.append(os.path.join(watersheds[i], d))
-
-    logging.basicConfig(filename=str(sys.argv[2]), level=logging.DEBUG)
-
+                # logging.info(os.path.join(watersheds[i], d))
+    logging.debug(dates)
     pool = mp.Pool()
     results = pool.map(extract_summary_table, dates)
 
